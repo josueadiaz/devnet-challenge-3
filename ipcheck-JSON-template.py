754a9a04c601ec1f05333ae3a5fac950cab590cd
Josue Diaz <jdiaz@presidio.com>
2020-04-20 14:30:53 -0400

    new files

---
+++ ipcheck-JSON-template.py
@@ -0,0 +1,22 @@
+"""
+Some lines are commented out.
+You may or may not want to use the ipaddress library
+but you will still need to check for a valid IPv4 adddress.
+
+The 'with open('JSONdata') as f:' line will read the data file
+AS LONG AS IT IS IN THE SAME DIRECTORY :)
+
+The next line should get you started with a method you will need
+from the json library to change the JSON text into something 
+you can call from Python
+"""
+
+
+#import ipaddress
+import json
+
+with open('JSONdata') as f:
+#    data = json.
+
+
+
